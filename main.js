024e0e68a9fbe047a3458bb9eba6f86d4e06187d
Sergey <Prostov>
2022-02-25 21:07:53 +0100

    initial

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow
+
 function createNewWindow () {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 500,
     height: 600,
-    title: 'Image Shrink'
+    // title: 'Image Shrink '
   });
+
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 }
 
 app.on('ready', createNewWindow)
