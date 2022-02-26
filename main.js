b7cb4a1adbefb80aa15c2ca6d965c061fa4b803d
Sergey <Prostov>
2022-02-26 12:15:29 +0100

    nodemon added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // set env
 process.env.NODE_ENV = 'development'
@@ -6,7 +6,9 @@ process.env.NODE_ENV = 'development'
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
-function createNewWindow () {
+let mainWindow
+
+function createMainWindow () {
   mainWindow = new BrowserWindow({
     width: 500,
     height: 600,
@@ -15,16 +17,21 @@ function createNewWindow () {
     resizable: isDev ? true : false
   });
 
-  app.on('window-all-closed', () => {
-    if (!isMac) app.quit()
-  })
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`)  // the same as:
   mainWindow.loadFile('./app/index.html')
 }
 
-app.on('ready', createNewWindow)
+app.on('ready', () => {
+  createMainWindow()
+  mainWindow.on('ready', ()=>{
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', () => {
+  if ( !isMac) app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+})
