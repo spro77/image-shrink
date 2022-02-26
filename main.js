7ffb77e166b622d5c63bc5d26ad0e9c9a9932a8b
Sergey <Prostov>
2022-02-26 11:47:29 +0100

    icon added; macc-specific behavior added

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const { app, BrowserWindow } = require('electron');
 
-let mainWindow
+// set env
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
 
 function createNewWindow () {
   mainWindow = new BrowserWindow({
     width: 500,
     height: 600,
-    // title: 'Image Shrink '
+    // title: 'Image Shrink'
+    icon: './assets/icons/Icon_256x256.png',
+    resizable: isDev ? true : false
   });
 
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  app.on('window-all-closed', () => {
+    if (!isMac) app.quit()
+  })
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`)  // the same as:
+  mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createNewWindow)
